c6d1d4a594953d982f4a62b76b1171f5d2ca8af3
Kevin Del Castillo <quebin31@gmail.com>
2021-03-03 18:05:30 -0500

    Add `build` static method for `AppDatabase`.

--- app/src/main/java/co/ravn/kevin/photosapp/database/AppDatabase.kt
+++ app/src/main/java/co/ravn/kevin/photosapp/database/AppDatabase.kt
@@ -1,6 +1,8 @@
 package co.ravn.kevin.photosapp.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import co.ravn.kevin.photosapp.database.dao.PhotoDao
 import co.ravn.kevin.photosapp.model.Photo
@@ -9,4 +11,11 @@ import co.ravn.kevin.photosapp.model.Photo
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun photoDao(): PhotoDao
+
+    companion object {
+        private const val DB_NAME = "PhotosApp"
+
+        fun build(context: Context): AppDatabase =
+            Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME).build()
+    }
 }
