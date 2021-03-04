6b60538fbda57785a795bd0b7c790f5257d042ac
Kevin Del Castillo <quebin31@gmail.com>
2021-03-04 13:13:33 -0500

    Optimize imports.

--- app/src/main/java/co/ravn/kevin/photosapp/hilt/NetworkingModule.kt
+++ app/src/main/java/co/ravn/kevin/photosapp/hilt/NetworkingModule.kt
@@ -10,7 +10,6 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Inject
 
 @Module
 @InstallIn(SingletonComponent::class)
