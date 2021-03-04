91b96a625005e58b3917747feae9b0509bf4ee65
Kevin Del Castillo <quebin31@gmail.com>
2021-03-04 11:10:41 -0500

    Add arguments delegated with `navArgs`.

--- app/src/main/java/co/ravn/kevin/photosapp/ui/photodetail/PhotoDetailFragment.kt
+++ app/src/main/java/co/ravn/kevin/photosapp/ui/photodetail/PhotoDetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import co.ravn.kevin.photosapp.databinding.FragmentPhotoDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,6 +15,7 @@ class PhotoDetailFragment : Fragment() {
 
     private var _binding: FragmentPhotoDetailBinding? = null
     private val binding get() = _binding!!
+    private val args by navArgs<PhotoDetailFragmentArgs>()
     private val viewModel by viewModels<PhotoDetailViewModel>()
 
     override fun onCreateView(
