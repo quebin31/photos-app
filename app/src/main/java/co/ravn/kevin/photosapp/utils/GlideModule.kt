9e1c66ffd55333e9527a9da6b422b8ca90ea18c0
Kevin Del Castillo <quebin31@gmail.com>
2021-03-04 17:39:54 -0500

    Add Glide app module.

---
+++ app/src/main/java/co/ravn/kevin/photosapp/utils/GlideModule.kt
@@ -0,0 +1,8 @@
+package co.ravn.kevin.photosapp.utils
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class GlideModule : AppGlideModule() {
+}
