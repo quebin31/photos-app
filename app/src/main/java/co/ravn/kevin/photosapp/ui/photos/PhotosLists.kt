283b03d35b69be2af52c0ce51c33c66cf52f2c2b
Kevin Del Castillo <quebin31@gmail.com>
2021-03-04 17:41:38 -0500

    Update code to add shared element transitions.
    Also add placeholder for `GlideApp` to show circular progress.

--- app/src/main/java/co/ravn/kevin/photosapp/ui/photos/PhotosLists.kt
+++ app/src/main/java/co/ravn/kevin/photosapp/ui/photos/PhotosLists.kt
@@ -1,15 +1,17 @@
 package co.ravn.kevin.photosapp.ui.photos
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import co.ravn.kevin.photosapp.databinding.ListItemPhotoBinding
 import co.ravn.kevin.photosapp.model.Photo
+import co.ravn.kevin.photosapp.utils.GlideApp
 import co.ravn.kevin.photosapp.utils.getMockUrlImage
-import com.bumptech.glide.Glide
 
 
-typealias OnClickCallback = (Photo) -> Unit
+typealias OnClickCallback = (Photo, Array<Pair<View, String>>) -> Unit
 
 class PhotoAdapter(private val onClick: OnClickCallback) : RecyclerView.Adapter<PhotoViewHolder>() {
 
@@ -36,9 +38,20 @@ class PhotoAdapter(private val onClick: OnClickCallback) : RecyclerView.Adapter<
 
 class PhotoViewHolder(private val binding: ListItemPhotoBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(photo: Photo, onClick: OnClickCallback) = with(binding) {
-        title.text = photo.title
-        Glide.with(itemView).load(photo.getMockUrlImage()).into(thumbnail)
+        val context = itemView.context
+        val transitionName = "photo-${photo.id}"
 
-        root.setOnClickListener { onClick(photo) }
+        title.text = photo.title
+        thumbnail.transitionName = transitionName
+
+        val circularProgressDrawable = CircularProgressDrawable(context)
+        circularProgressDrawable.strokeWidth = 5f
+        circularProgressDrawable.centerRadius = 30f
+        circularProgressDrawable.start()
+
+        GlideApp.with(context).load(photo.getMockUrlImage()).placeholder(circularProgressDrawable).into(thumbnail)
+
+        val imageTransition = thumbnail as View to transitionName
+        root.setOnClickListener { onClick(photo, arrayOf(imageTransition)) }
     }
 }
