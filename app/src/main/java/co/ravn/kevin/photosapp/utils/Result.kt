522fb6ba68f720de3d5d73007442eaa882e3cabe
Kevin Del Castillo <quebin31@gmail.com>
2021-03-04 10:15:04 -0500

    Add `Result` type to run operations safely.

---
+++ app/src/main/java/co/ravn/kevin/photosapp/utils/Result.kt
@@ -0,0 +1,17 @@
+package co.ravn.kevin.photosapp.utils
+
+sealed class Result<out T> {
+    data class Ok<out T>(val value: T) : Result<T>()
+    data class Err<out E>(val error: E) : Result<Nothing>()
+}
+
+fun <T> T.toResult(): Result<T> = Result.Ok(this)
+fun <E: Throwable> E.toResult(): Result<Nothing> = Result.Err(this)
+
+suspend inline fun <T, R> T.runToResult(crossinline block: suspend T.() -> R): Result<R> {
+    return try {
+        Result.Ok(block())
+    } catch (e: Throwable) {
+        Result.Err(e)
+    }
+}
