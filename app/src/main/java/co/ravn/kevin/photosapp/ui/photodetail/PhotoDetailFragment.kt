3a76b5c908c121accaf693906c33c1911cf54b02
Kevin Del Castillo <quebin31@gmail.com>
2021-03-04 17:44:58 -0500

    Update transition logic for views and fragment.
    - Set the enter and shared element transition for this fragment.
    - Set the transition name for image view based on the photo id.
    - Postpone until the image data is ready (showing a circular progress bar).

--- app/src/main/java/co/ravn/kevin/photosapp/ui/photodetail/PhotoDetailFragment.kt
+++ app/src/main/java/co/ravn/kevin/photosapp/ui/photodetail/PhotoDetailFragment.kt
@@ -1,14 +1,21 @@
 package co.ravn.kevin.photosapp.ui.photodetail
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import co.ravn.kevin.photosapp.databinding.FragmentPhotoDetailBinding
+import co.ravn.kevin.photosapp.utils.GlideApp
 import co.ravn.kevin.photosapp.utils.getMockUrlImage
-import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -31,7 +38,16 @@ class PhotoDetailFragment : Fragment() {
         _binding = null
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val inflater = TransitionInflater.from(requireContext())
+        enterTransition = inflater.inflateTransition(android.R.transition.fade)
+        sharedElementEnterTransition = inflater.inflateTransition(android.R.transition.move)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        postponeEnterTransition()
         initUi()
     }
 
@@ -39,7 +55,41 @@ class PhotoDetailFragment : Fragment() {
         val context = requireContext()
         val photo = args.photo
 
+        fullImage.transitionName = "photo-${args.photo.id}"
         title.text = photo.title
-        Glide.with(context).load(photo.getMockUrlImage()).into(fullImage)
+
+        val circularProgressDrawable = CircularProgressDrawable(context)
+        circularProgressDrawable.strokeWidth = 5f
+        circularProgressDrawable.centerRadius = 30f
+        circularProgressDrawable.start()
+
+        GlideApp
+            .with(context)
+            .load(photo.getMockUrlImage())
+            .placeholder(circularProgressDrawable)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    startPostponedEnterTransition()
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    startPostponedEnterTransition()
+                    return false
+                }
+
+            })
+            .into(fullImage)
     }
 }
