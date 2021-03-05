bd53ab7a4932f3f2171252cd101f0fb37364fafe
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-03-05 15:46:55 -0500

    Simplify return type from `Uri` to `String`.

--- app/src/main/java/co/ravn/kevin/photosapp/utils/PhotoUtils.kt
+++ app/src/main/java/co/ravn/kevin/photosapp/utils/PhotoUtils.kt
@@ -1,8 +1,5 @@
 package co.ravn.kevin.photosapp.utils
 
-import android.net.Uri
 import co.ravn.kevin.photosapp.model.Photo
 
-fun Photo.getMockUrlImage(): Uri {
-    return Uri.parse("https://picsum.photos/id/$id/600")
-}
+fun Photo.getMockUrlImage(): String = "https://picsum.photos/id/$id/600"
