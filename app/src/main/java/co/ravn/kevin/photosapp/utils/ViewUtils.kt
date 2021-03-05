ef91dc272db0214ebdf00ec427a91538434b1083
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-03-05 18:07:47 -0500

    Add custom binding adapter to `ImageView` for loading images with `Glide`.
    This sets a circular progress drawable as placeholder while the image is loading, also receives a boolean for this specific case to indicate whether the fragment transition has been postponed and should be started once the glide finishes its work.

--- app/src/main/java/co/ravn/kevin/photosapp/utils/ViewUtils.kt
+++ app/src/main/java/co/ravn/kevin/photosapp/utils/ViewUtils.kt
@@ -1,6 +1,16 @@
 package co.ravn.kevin.photosapp.utils
 
+import android.graphics.drawable.Drawable
 import android.view.View
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.findFragment
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 fun View.visible() {
     visibility = View.VISIBLE
@@ -8,4 +18,48 @@ fun View.visible() {
 
 fun View.gone() {
     visibility = View.GONE
+}
+
+
+@BindingAdapter("imageUrl", "transitionIsPostponed")
+fun loadImageLoading(imageView: ImageView, url: String, transitionIsPostponed: Boolean) {
+    val circularProgressDrawable = CircularProgressDrawable(imageView.context)
+    circularProgressDrawable.strokeWidth = 5f
+    circularProgressDrawable.centerRadius = 30f
+    circularProgressDrawable.start()
+
+    // This may be right now, but not all ImageViews are inside a fragment.
+    val fragment by lazy {  imageView.findFragment<Fragment>() }
+    val maybeStartTransition = {
+        if (transitionIsPostponed)
+            fragment.startPostponedEnterTransition()
+    }
+
+    GlideApp
+        .with(imageView)
+        .load(url)
+        .placeholder(circularProgressDrawable)
+        .listener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                maybeStartTransition()
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                maybeStartTransition()
+                return false
+            }
+        })
+        .into(imageView)
 }
