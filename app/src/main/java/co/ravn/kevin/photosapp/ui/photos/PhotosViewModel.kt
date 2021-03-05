9b08874386d59b274d5fbcacec60d967b9e67211
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-03-05 18:08:14 -0500

    Add extra live data value to indicate the visibility of the `no_data` text view.

--- app/src/main/java/co/ravn/kevin/photosapp/ui/photos/PhotosViewModel.kt
+++ app/src/main/java/co/ravn/kevin/photosapp/ui/photos/PhotosViewModel.kt
@@ -1,7 +1,9 @@
 package co.ravn.kevin.photosapp.ui.photos
 
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 import co.ravn.kevin.photosapp.model.Photo
 import co.ravn.kevin.photosapp.repository.AppRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,5 +11,12 @@ import javax.inject.Inject
 
 @HiltViewModel
 class PhotosViewModel @Inject constructor(private val repository: AppRepository) : ViewModel() {
+
     val photos: LiveData<List<Photo>> by lazy { repository.getPhotos() }
+
+    val noDataIsVisible: LiveData<Int> by lazy {
+        photos.map {
+            if (it.isEmpty()) View.VISIBLE else View.GONE
+        }
+    }
 }
